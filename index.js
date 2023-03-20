1871cd068e71ad4ea6aec849779c610dec7cce4b
Akeem Lawrence <akeemwlawrence@gmail.com>
2023-03-20 15:12:06 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+/*
+function isCherries(fruit) {
+    return fruit.name === "cherries";
+  }
+
+console.log(inventory.find(isCherries));
+
+function isW(season) {
+    return season.result === "W";
+  }
+*/
+
+function superbowlWin(array) {
+    for(const season of array) {
+        if(season.result === "W") {
+            return season.year;
+        }
+    }
+}
