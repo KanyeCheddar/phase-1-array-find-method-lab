e2d39d3c6e621190c8ba387e85fdcc66e3808328
Akeem Lawrence <akeemwlawrence@gmail.com>
2023-03-20 19:27:36 -0400

    solved lab

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 // code your solution here
+/*
 const record = [
     { year: "2015", result: "W"},
     { year: "2014", result: "N/A"},
     { year: "2013", result: "L"},
     //...
   ]
-
-/*
-function isCherries(fruit) {
-    return fruit.name === "cherries";
-  }
-
-console.log(inventory.find(isCherries));
-
-function isW(season) {
-    return season.result === "W";
-  }
 */
 
-function superbowlWin(array) {
-    for(const season of array) {
-        if(season.result === "W") {
-            return season.year;
+const record =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+
+
+/*
+function isW(season) {
+    for(const element in season){
+        if(result === "W") {
+            return element.year;
         }
     }
 }
+*/
+
+function superbowlWin(record) {
+    const isChampion = record.find(championship => championship.result === "W");
+    if(isChampion) {
+        return isChampion.year;
+    } else {
+        return isChampion;
+    }
+}
+
+console.log(superbowlWin(record))
